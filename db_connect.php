18e92f0df265e18dbec7b14a0360191c87237ff2
SimonToroni <145996021+SimonToroni@users.noreply.github.com>
2026-06-02 09:14:27 +0200

    Refactor db_connect.php for improved readability

--- db_connect.php
+++ db_connect.php
@@ -14,7 +14,6 @@ $options = [
 
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
-     // echo "Připojení k dbs.spskladno.cz proběhlo úspěšně!";
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
